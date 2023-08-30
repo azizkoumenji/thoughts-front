b7321a0e68583e2be59cd56a0b45e10084d2afd0
Mohamed Aziz Koumenji <113291493+mohamedazizkoumenji@users.noreply.github.com>
2023-08-30 22:39:27 +0100

    Added automatic dark/light theme functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Landing from "./Components/Landing/Landing";
 
 function App() {
+  let [mode, setMode] = useState("light");
+
+  useEffect(() => {
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? setMode("dark")
+      : setMode("light");
+
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (event) => {
+        const colorScheme = event.matches ? "dark" : "light";
+        setMode(colorScheme);
+      });
+  }, []);
+
+  console.log(mode);
+
+  let classes = "";
+  if (mode === "dark") {
+    classes = "dark text-foreground bg-background min-h-screen";
+  } else {
+    classes = "min-h-screen";
+  }
+  console.log(classes);
+
   return (
-    <>
+    <main className={classes}>
       <Landing />
-    </>
+    </main>
   );
 }
 
