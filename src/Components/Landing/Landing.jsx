6dc308e58bcee0552fac053cf1254431cdf2a064
Mohamed Aziz Koumenji <113291493+mohamedazizkoumenji@users.noreply.github.com>
2023-08-30 14:20:00 +0100

    Initial commit with a logo

---
+++ src/Components/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+import { Logo } from "../Icons/Logo";
+
+const Landing = () => {
+  return (
+    <div className="flex min-h-screen">
+      <Logo />
+    </div>
+  );
+};
+
+export default Landing;
